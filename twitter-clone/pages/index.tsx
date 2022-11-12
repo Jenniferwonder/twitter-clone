8c9a9b4c526c859135b7f4df9b071af0025de531
Jenniferwonder <amazing_coder@163.com>
2022-11-12 22:45:20 +0800

    Add Sidebar

---
+++ twitter-clone/pages/index.tsx
@@ -0,0 +1,24 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import Sidebar from "../components/Sidebar";
+
+const Home: NextPage = () => {
+	return (
+		<div>
+			<Head>
+				<title>Twitter 2.0</title>
+			</Head>
+			<main>
+				{/* Sidebar */}
+				<Sidebar />
+
+				{/* Feed */}
+
+				{/* Widgets */}
+			</main>
+		</div>
+	);
+};
+
+export default Home;
